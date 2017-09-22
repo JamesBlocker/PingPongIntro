afcc7c83b11ae8ba129c5ac6b848f407f31faded
Byron Blocker <byronblocker@gmail.com>
2017-09-22 13:55:37 -0700

    add second form and reverse ping function

--- js/scripts.js
+++ js/scripts.js
@@ -14,17 +14,42 @@ var ping = function(num) {
     return num;
 };
 
+var pong = function(num) {
+    for (var i = 1; i <= num; i++) {
+        if (i % 15 === 0) {
+            $('#list').prepend('<li>Ping Pong Pandemonium</li>');
+        } else if (i % 5 === 0) {
+            $('#list').prepend('<li>Pong</li>');
+        } else if (i % 3 === 0) {
+            $('#list').prepend('<li>Ping</li>');
+        } else {
+            $('#list').prepend('<li>' + i +'</li>');
+        }
+    }
+    return num;
+};
+
 
 
 
             <!--front end-->
 
 $(document).ready(function() {
-    $('#form').submit(function(event) {
+    $('#form1').submit(function(event) {
         var userInput = $('#userInput').val();
         $('#list').html("");
         $('#pong').fadeOut();
         var result = ping(userInput);
+
+        $('.output').show();
+        event.preventDefault();
+    });
+
+    $('#form2').submit(function(event) {
+        var userInput = $('#userInput2').val();
+        $('#list').html("");
+        $('#pong').fadeOut();
+        var result = pong(userInput);
 
         $('.output').show();
         event.preventDefault();
