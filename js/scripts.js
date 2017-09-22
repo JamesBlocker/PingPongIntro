7eb8099dce7bda941ac5b835f5b1cebfd154e7fe
Byron Blocker <byronblocker@gmail.com>
2017-09-22 09:11:24 -0700

    add logic to display ping pong list

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
             <!--back end-->
 var ping = function(num) {
     for (var i = 1; i < num; i++) {
-        $('#list').append('<li>' + i +'</li>');
+        if (i % 15 === 0) {
+            $('#list').append('<li>Ping Pong Pandemonium</li>');
+        } else if (i % 5 === 0) {
+            $('#list').append('<li>Pong</li>');
+        } else if (i % 3 === 0) {
+            $('#list').append('<li>Ping</li>');
+        } else {
+            $('#list').append('<li>' + i +'</li>');
         }
+
+    }
 
     return num;
 };
